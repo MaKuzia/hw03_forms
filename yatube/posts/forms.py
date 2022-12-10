ae2d0b6df686d3eec89814fc4c2f435ffca04d4e
Марина <matinawerry@gmail.com>
2022-12-10 10:32:42 +0300

    Final ver_1

---
+++ yatube/posts/forms.py
@@ -0,0 +1,22 @@
+from django import forms
+
+from .models import Post
+
+
+class PostForm(forms.ModelForm):
+    text = forms.CharField(widget=forms.Textarea)
+
+    class Meta:
+        model = Post
+        fields = ('text', 'group')
+
+    def __init__(self, *args, **kwargs):
+        super(PostForm, self).__init__(*args, **kwargs)
+        self.fields['text'].widget.attrs['cols'] = 92
+        self.fields['text'].widget.attrs['rows'] = 10
+
+    def clean_text(self):
+        data = self.cleaned_data['text']
+        if data == '':
+            raise forms.ValidationError('А кто поле будет заполнять, Пушкин?')
+        return data
---
+++ yatube/posts/views.py
@@ -0,0 +1,99 @@
+from django.conf import settings
+from django.contrib.auth import get_user_model
+from django.shortcuts import redirect
+from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from django.core.paginator import Paginator
+
+from .forms import PostForm
+from .models import Group, Post
+
+User = get_user_model()
+
+
+def ForPaginator(request, list_object):
+    paginator = Paginator(list_object, settings.POSTS_PER_PAGE)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    return page_obj
+
+
+def index(request):
+    template = 'posts/index.html'
+    context = {
+        'page_obj': ForPaginator(request, Post.objects.all()),
+    }
+    return render(request, template, context)
+
+
+def group_posts(request, slug):
+    template = 'posts/group_list.html'
+    group = get_object_or_404(Group, slug=slug)
+    posts_group = group.posts.all()
+    context = {
+        'group': group,
+        'posts_group': posts_group,
+        'page_obj': ForPaginator(request, posts_group),
+    }
+    return render(request, template, context)
+
+
+def profile(request, username):
+    template = 'posts/profile.html'
+    author = get_object_or_404(User, username=username)
+    user_posts = author.posts.all()
+    context = {
+        'author': author,
+        'user_posts': user_posts,
+        'page_obj': ForPaginator(request, user_posts),
+    }
+    return render(request, template, context)
+
+
+def post_detail(request, post_id):
+    template = 'posts/post_detail.html'
+    post_req = get_object_or_404(Post, pk=post_id)
+    author = Post.objects.filter(author=post_req.author)
+    context = {
+        'post': post_req,
+        'author': author,
+    }
+    return render(request, template, context)
+
+
+def post_create(request):
+    form = PostForm
+    template = 'posts/create_post.html'
+    if request.method == 'POST':
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(False)
+            post.author = request.user
+            post.save()
+            return redirect('posts:profile', post.author.username)
+        context = {
+            'form': form,
+        }
+        return render(request, template, context)
+    form = PostForm()
+    return render(request, template, {'form': form})
+
+
+def post_edit(request, post_id):
+    current_post = get_object_or_404(Post, pk=post_id)
+    template = 'posts/create_post.html'
+    is_edit = True
+    if request.method == 'POST':
+        form = PostForm(request.POST, instance=current_post)
+        if form.is_valid():
+            edit_post = form.save(False)
+            current_post.text = edit_post.text
+            current_post.group = edit_post.group
+            current_post.save(update_fields=['text', 'group'])
+            return redirect('posts:post_detail', current_post.pk)
+    form = PostForm(instance=current_post)
+    context = {
+        'form': form,
+        'is_edit': is_edit,
+    }
+    return render(request, template, context)
